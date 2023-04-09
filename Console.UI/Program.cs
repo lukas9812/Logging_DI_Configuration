3ac57a7fa8812303fb51132eb34652197b46d426
Lukáš Srovnal <srovnal@cngroup.dk>
2023-04-09 21:31:51 +0200

    adding files

---
+++ Console.UI/Program.cs
@@ -0,0 +1,16 @@
+﻿//DI, Serilog, Settings
+using Microsoft.Extensions.Configuration;
+
+var builder = new ConfigurationBuilder();
+BuildConfig(builder);
+
+
+static void BuildConfig(IConfigurationBuilder builder)
+{
+    //This talking to application configuration source.
+    builder.SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json",
+            optional: true)
+        .AddEnvironmentVariables();
+}
